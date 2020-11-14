ad297763b8f2fa098cb76ae8810331f62369d070
dhyeysolanki <71046617+dhyeysolanki@users.noreply.github.com>
2020-11-13 19:04:39 -0500

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,25 @@
+class Block {
+
+    constructor (x, y, w, h){
+
+        this.width = w
+        this.height = h
+
+        var options = {
+            restitution : 0.4,
+            friction : 0
+        }
+        this.body = Bodies.rectangle (x, y, w, h, options);
+        World.add (world, this.body);
+    }
+
+    display (){
+
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(this.body.angle);
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+    }
+}
